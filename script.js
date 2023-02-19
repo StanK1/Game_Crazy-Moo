d78eb9574f960f9995f284cead7491dcebb1f69e
Stanislav <stanislavkrstev8@gmail.com>
2023-02-20 00:45:10 +0200

    Design changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let canvas = document.getElementById("gameCanvas");
 
+
+
 let sqsize = 50;
 let canvasWidth = 10;
 let canvasHeight = 5;
@@ -7,9 +9,16 @@ canvas.width = canvasWidth * sqsize;
 canvas.height = canvasHeight * sqsize;
 
 let context = canvas.getContext("2d");
-let herox = 1;
+//Hero start position
+let herox = 4;
 let heroy = 2;
 
+let candyCount = 0;
+let imgC = new Image();
+imgC.src = "Candy.png"
+
+let img = new Image();
+img.src = "Herobig.png"
 
 function rand(min, max) {
     return min + Math.random() * (max - min);
@@ -22,78 +31,159 @@ function get_random_color() {
     return 'hsl(' + h + ',' + s + '%,' + l + '%)';
 }
 
+let candies = [];
 
-
-let candy = Math.floor(Math.random() * 10);
-let imgC = new Image();
-imgC.src = "Candy.png"
-
-let img = new Image();
-img.src = "Herobig.png"
-
-//context.drawImage(img, herox * sqsize, heroy * sqsize, sqsize, sqsize);
-	
-function drawMap()	{
-	for (let i = 0; i < canvasWidth; i++) {
-		for (let j = 0; j < canvasHeight; j++) {
-			context.fillStyle = get_random_color();
-			context.fillRect(i * sqsize, j * sqsize, sqsize, sqsize);
-			context.strokeRect(i * sqsize, j * sqsize, sqsize, sqsize);
-			
-		}
-	}
+function spawnCandy() {
+    candyX = get_randomXY(9);
+    candyY = get_randomXY(4);
+    candies.push({x: candyX, y: candyY});
 }
+
+function drawMap() {
+    for (let i = 0; i < canvasWidth; i++) {
+        for (let j = 0; j < canvasHeight; j++) {
+            context.fillStyle = get_random_color();
+            context.fillRect(i * sqsize, j * sqsize, sqsize, sqsize);
+            context.strokeRect(i * sqsize, j * sqsize, sqsize, sqsize);
+        }
+    }
+}
+
+function drawHero() {
+    context.drawImage(img, herox * sqsize, heroy * sqsize, sqsize, sqsize);
+}
+
+function drawCandies() {
+    for (let candy of candies) {
+        context.drawImage(imgC, candy.x * sqsize, candy.y * sqsize, sqsize, sqsize);
+    }
+}
+
+function checkCandyCollision() {
+    for (let i = 0; i < candies.length; i++) {
+        if (herox === candies[i].x && heroy === candies[i].y) {
+            candies.splice(i, 1);
+            candyCount++;
+            if (candyCount === 10) {
+                alert("You WIN");
+                let tryAgain = confirm("Do you want to try again?");
+                if (tryAgain) {
+                    location.reload();
+                }
+                return;
+            } else {
+                spawnCandy();
+            }
+        }
+    }
+}
+
+function moveUp() {
+    if (isWithinCanvas(herox, heroy - 1)) { 
+        heroy--;
+        checkCandyCollision();
+        drawMap();
+        drawHero();
+        drawCandies();
+    }
+}
+
+function moveDown() {
+    if (isWithinCanvas(herox, heroy + 1)) {
+        heroy++;
+        checkCandyCollision();
+        drawMap();
+        drawHero();
+        drawCandies();
+    }
+}
+
+function moveLeft() {
+    if (isWithinCanvas(herox - 1, heroy)) {
+        herox--;
+        checkCandyCollision();
+        drawMap();
+        drawHero();
+        drawCandies();
+    }
+}
+
+function moveRight() {
+    if (isWithinCanvas(herox + 1, heroy)) {
+        herox++;
+        checkCandyCollision();
+        drawMap();
+        drawHero();
+        drawCandies();
+    }
+}
+
+document.onkeypress = function(e) {
+    let key = e.key;
+    switch (key) {
+        case "w":
+            moveUp();
+            break;
+        case "s":
+            moveDown();
+            break;
+        case "a":
+            moveLeft();
+            break;
+        case "d":
+            moveRight();
+            break;
+    }
+}
+
+function get_randomXY(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function isWithinCanvas(x, y) {
+    return x >= 0 && x < canvasWidth && y >= 0 && y < canvasHeight;
+}
+
+let timeLeft = 15; 
+
+let timerElement = document.getElementById("timer"); 
+
+function countdown() {
+    timeLeft--;
+    timerElement.innerHTML = "Time left: " + timeLeft;
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        alert("Time's up!");
+        let tryAgain = confirm("Do you want to try again?");
+        if (tryAgain) {
+            location.reload();
+        }
+    }
+}
+
+let timer = setInterval(countdown, 1000);
+
+function checkCandyCollision() {
+    for (let i = 0; i < candies.length; i++) {
+        if (herox === candies[i].x && heroy === candies[i].y) {
+            candies.splice(i, 1);
+            candyCount++;
+            if (candyCount === 10) {
+                clearInterval(timer); // stop the timer when all the candies are collected
+                alert("You WIN");
+                let tryAgain = confirm("Do you want to try again?");
+                if (tryAgain) {
+                    location.reload();
+                }
+                return;
+            } else {
+                spawnCandy();
+            }
+        }
+    }
+}
+
+spawnCandy();
 drawMap();
-function drawHero(){
-	context.drawImage(img, herox * sqsize, heroy * sqsize, sqsize, sqsize);
-}
-canvas.onclick = function(e) {
-	let mousePosX = e.x - e.originalTarget.offsetLeft;
-	let mousePosY = e.y - e.originalTarget.offsetTop;
-	herox = Math.floor(mousePosX / sqsize);
-	heroy = Math.floor(mousePosY / sqsize);
-
-	drawMap();
-	drawHero();
-}
-
-function drawCandy(){
-	context.drawImageC(imgC, herox * sqsize, heroy * sqsize, sqsize, sqsize);
-}
-
-
-
-
-function moveUp(){
- heroy--;
- drawMap();
-	drawHero();
-}
-
-function moveDown(){
- heroy++;
- drawMap();
-	drawHero();
-}
-
-function moveLeft(){
- herox--;
- drawMap();
-	drawHero();
-}
-
-function moveRight(){
- herox++;
- drawMap();
-	drawHero();
-}
-
-document.onkeypress = function(e){
-let key = e.key;
-switch(key){
-	case "w": moveUp(); break;
-	case "s": moveDown(); break;
-	case "a": moveLeft(); break;
-	case "d": moveRight(); break;
-}
-}
+drawHero();
+drawCandies();
